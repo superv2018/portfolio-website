e65f77c7f7313dfcc82c37c13ad2bcc4af746f6a
superv2018 <pelumiv@yahoo.com>
2022-02-07 15:26:51 +0200

    edited header to full stack developer

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     return (
         <section className="header">
             <h1>Victor Awakan</h1>
-            <h2>Junior Full Developer</h2>
+            <h2>Junior Full Stack Developer</h2>
             <h3>JavaScript, TypeScript, React, Node, AWS, HTML and CSS</h3>
             
         </section>
